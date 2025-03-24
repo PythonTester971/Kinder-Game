3e777ccb2e630bd2bd369560d8c41815117aebcb
PythonTester <gdenisse971@outlook.fr>
2025-03-24 16:42:18 +0100

    Création navbar admin et modification tableaux de bord

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 
-#[Route('/shop')]
+
 final class ShopController extends AbstractController
 {
     #[Route('', name: 'shop_index')]
